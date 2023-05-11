f7efaf0bb8a827b5dc54c5445a6cc6f3f519b1ea
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-05-11 14:54:43 -0300

    update

--- bus.c/includes/bus/bus.h
+++ bus.c/includes/bus/bus.h
@@ -1,8 +1,19 @@
 typedef struct bus Bus;
 
+//inicia a lista de onibus
 Bus *startBus();
+
+//Função para registrar o onibus
 Bus *registerBus(Bus *b, int number, int vacancies, char *origin, char *destination);
+
+//Função para mostrar onibus
 void showBus(Bus *b);
+
+//Função para mostrar as vagas dos onibus
 void showAvaliableVacancies(Bus *b, int num);
+
+//Função que mostrar a quantidade de passageiros cadastrados
 void showPassengerQuantity(Bus *b, int vacancies, int num);
+
+//Função para liberar o espaço que foi alocado na memoria
 void freeBus(Bus *b);
--- bus.c/includes/reservation/reservation.h
+++ bus.c/includes/reservation/reservation.h
@@ -1,14 +1,35 @@
 #include "../bus/bus.h"
 typedef struct tickets Tickets;
 
+//Inicia a lista de reservas
 Tickets *start();
+
+//Função para verificar se há espaço desnecesário
 void hasDuplicateSpace(char *name);
+
+//Função para formatar texto
 void formatText(char *name);
+
+//Função para criar a reserva
 Tickets *makeReservation(Tickets *l, Bus *b, int number, char *name);
+
+//Função para excluir reserva
 Tickets *deleteReservation(Tickets *t, Bus *b, char *name);
+
+//Função para mostrar a reserva
 void showReservation(Tickets *t, int number);
+
+//Função para buscar uma reserva
 void searchReservation(Tickets *t, char *name);
+
+//Função para editar reserva
 void editReservation(Tickets *t, Bus *b, int number);
+
+//Função para escrever a reserva no arquivo
 void writeFile(Tickets *t);
+
+//Função para ler o arquivo
 Tickets *readFile(Tickets *t, Bus *b);
+
+//Função para liberar o espaço de memoria que foi alocado
 void freeTickets(Tickets *t);
