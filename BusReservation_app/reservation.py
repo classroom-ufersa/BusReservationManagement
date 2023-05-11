b1c1628b52139e694ea1dbd7b4d4d83b4ef2b262
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-05-11 13:50:18 -0300

    up

--- BusReservation_app/reservation.py
+++ BusReservation_app/reservation.py
@@ -7,18 +7,18 @@ def realizar_reserva(input_name, input_numBus, num_ticket):
               f'Número do ticket: {num_ticket}\n'
 
     #Ler as reservas existentes do arquivo
-    reservas = []
+    reservas = [] # Cria uma lista vazia chamada "reservas"
     with open('tickets.txt', 'r') as file:
         reserva_atual = ''
         for line in file:
-            line = line.strip()
-            if line == '':
-                reservas.append(reserva_atual)
-                reserva_atual = ''
+            line = line.strip() #Remove espaços em branco extras do início e do fim da linha.
+            if line == '': #Verifica se a linha está vazia. Isso indica o fim de uma reserva.
+                reservas.append(reserva_atual) #Adiciona a reserva atual à lista de reservas.
+                reserva_atual = '' #Limpa a variável "reserva_atual" para armazenar a próxima reserva.
             else:
-                reserva_atual += line + '\n'
+                reserva_atual += line + '\n' #Adiciona a linha atual à variável "reserva_atual".
 
-    reservas.append(reserva) #Adicionar nova reserva à lista
+    reservas.append(reserva) #Adicionar nova reserva à lista reservas
     reservas.sort() #Ordenar a lista em ordem alfabética dos nomes
 
     with open('tickets.txt', 'w') as file: #Escrever as reservas ordenadas de volta ao arquivo
@@ -26,15 +26,15 @@ def realizar_reserva(input_name, input_numBus, num_ticket):
             file.write(reserva + '\n')
 def excluir_reserva(nome):
     with open('tickets.txt', 'r') as file:
-        lines = file.readlines()
+        lines = file.readlines() #Lê o arquivo por linha
     
     with open('tickets.txt', 'w') as file:
         deleted = False
-        for i in range(0, len(lines), 4):
-            if lines[i].strip() == f'Nome do passageiro: {nome}':
+        for i in range(0, len(lines), 4): #itera sobre os indices da lista 'lines' com o encremento de +4
+            if lines[i].strip() == f'Nome do passageiro: {nome}': #verifica se a linha atual possui o nome fornecido
                 deleted = True
-                continue
-            file.write(lines[i])
+                continue #Pula para a próxima iteração do loop
+            file.write(lines[i]) #escreve a linha atual(nome do passageiro) no arquivo
             file.write(lines[i+1])
             file.write(lines[i+2])
             file.write(lines[i+3])
@@ -51,7 +51,7 @@ def listar_reservas():
         st.write("Não há reservas registradas.")
     else:
         for i in range(0, len(lines), 4):
-            nome = lines[i].strip().split(': ')[1]
+            nome = lines[i].strip().split(': ')[1] #Divide a linha em duas partes usando o separador ': '
             num_bus = lines[i+1].strip().split(': ')[1]
             num_ticket = lines[i+2].strip().split(': ')[1]
             st.write(f"Nome do passageiro: {nome}")
@@ -87,7 +87,6 @@ def editar_reserva(nome, new_bus,new_num_ticket):
         edited = False
         for i in range(0, len(lines), 4):
             if lines[i].strip() == f'Nome do passageiro: {nome}':
-
                 # Escrever os novos dados da reserva no arquivo
                 file.write(f'Nome do passageiro: {nome}\n')
                 file.write(f'Número do ônibus selecionado: {new_bus}\n')
